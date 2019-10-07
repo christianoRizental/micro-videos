8043ac310bb95df82ed4e0b5750e3f0a2bdcc2ac
Christiano Rizental <rizental@gmail.com>
2019-10-07 16:35:52 -0300

    Criando recurso Category

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['namespace' => 'Api'], function() {
+    $exceptCreatAndEdit = [
+        'except' => ['create','edit']
+    ];
+    Route::resource('categories','CategoryController',$exceptCreatAndEdit);
+    Route::resource('genres','GenreController', $exceptCreatAndEdit);
+});
